0264b725774f4f5fbc41742957bf9e9954e29fd7
VladyslavStarina <46134124+VladyslavStarina@users.noreply.github.com>
2019-06-26 12:22:16 +0300

    Create test.hpp

---
+++ test.hpp
@@ -0,0 +1,23 @@
+#ifndef _TEST_HPP_
+#define _TEST_HPP_
+
+#include "integer_deque.hpp"
+
+void test1(IntegerDeque & _directory);
+void test2(IntegerDeque & _directory);
+void test3(IntegerDeque & _directory);
+void test4(IntegerDeque & _directory);
+void test5(IntegerDeque & _directory);
+void test6(IntegerDeque & _directory);
+void test7(IntegerDeque & _directory);
+void test8(IntegerDeque & _directory);
+void test9(IntegerDeque & _directory);
+void test10(IntegerDeque & _directory);
+void test11(IntegerDeque & _directory);
+void test12(IntegerDeque & _directory);
+void test13(IntegerDeque & _directory);
+void test14(IntegerDeque & _directory);
+void test15(IntegerDeque & _directory);
+void test16(IntegerDeque & _directory);
+
+#endif

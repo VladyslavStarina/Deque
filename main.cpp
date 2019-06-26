5e52e3b6b1539e0262b7fefe81f18b45d39ff58f
VladyslavStarina <46134124+VladyslavStarina@users.noreply.github.com>
2019-06-26 12:24:00 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "test.hpp"
+
+int main()
+{
+	IntegerDeque directory;
+
+	test1(directory);
+	test2(directory);
+	test3(directory);
+	test4(directory);
+	test5(directory);
+	test6(directory);
+	test7(directory);
+	test8(directory);
+	test9(directory);
+	test10(directory);
+	test11(directory);
+	test12(directory);
+	test13(directory);
+	test14(directory);
+	test15(directory);
+	test16(directory);
+
+	return 0;
+}
